5319ac07eeb2fe43971c8a15d03e358f3bd5e683
Adif <adif.ladiva@gmail.com>
2020-08-22 22:29:05 +0700

    Edit skills list into new file

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,38 +1,15 @@
 import React from 'react'
 import SkillsContent from './SkillsContent'
+import skillsList from '../contents/Skills'
 import './Skills.css'
 
 class Skills extends React.Component{
   render(){
-    //OBJECT FOR SKILL LIST
-    const skillList = [
-      {
-        id: '1',
-        name: 'Programming',
-        items: ['Python', 'C', 'C++']
-      },
-      {
-        id: '2',
-        name: 'Embedded System',
-        items: ['Arduino', 'Eagle' ,'ESP32' ,'Computer Vision (OpenCV)', 'Raspberry Pi']
-      },
-      {
-        id: '3',
-        name: 'Web Development',
-        items: ['HTML', 'CSS', 'JavaScript','Node.js', 'React.js', 'MySQL']
-      },
-      {
-        id: '4',
-        name: 'Other',
-        items: ['Adobe Photoshop', 'Linux', 'Git']
-      },
-    ];
-
     return(
       <div className="container-fluid skills-section">
         <h1 className="title"><span className="sign">&#187;</span>SKILLS<span className="sign">&#171;</span></h1>
         <div className="row row-cols-1 row-cols-md-4 px-lg-5">
-          {skillList.map((skillItem)=>{
+          {skillsList.map((skillItem)=>{
             return(
               <SkillsContent
                 key = {skillItem.id}
---
+++ src/contents/Skills.js
@@ -0,0 +1,24 @@
+const skillsList = [
+    {
+      id: '1',
+      name: 'Programming',
+      items: ['Python', 'C', 'JavaScript', 'C++']
+    },
+    {
+      id: '2',
+      name: 'Embedded System',
+      items: ['Arduino', 'Eagle' ,'ESP32' ,'Computer Vision (OpenCV)', 'Raspberry Pi']
+    },
+    {
+      id: '3',
+      name: 'Web Development',
+      items: ['HTML', 'CSS','Node.js', 'React.js', 'MySQL', 'MongoDB', "Docker"]
+    },
+    {
+      id: '4',
+      name: 'Other',
+      items: ['Adobe Photoshop', 'Linux', 'Git']
+    },
+  ];
+
+export default skillsList
