f067b0654ad1d0f700e715179c01248bd96670e2
Adif <adif.ladiva@gmail.com>
2020-08-23 14:45:09 +0700

    Updates

---
+++ precache-manifest.7d3ecbbd1e562494fead045ae2ef671b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6f2b98a69c185f4e85b0d79fad299a",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "fb40178801ccc9002834",
+    "url": "/profile-page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1dfcc3b4d2c1e5eb6dc",
+    "url": "/profile-page/static/css/main.5ea4c4d6.chunk.css"
+  },
+  {
+    "revision": "fb40178801ccc9002834",
+    "url": "/profile-page/static/js/2.4f839506.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/profile-page/static/js/2.4f839506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1dfcc3b4d2c1e5eb6dc",
+    "url": "/profile-page/static/js/main.31ad34a9.chunk.js"
+  },
+  {
+    "revision": "0b487b503f1c32386e01",
+    "url": "/profile-page/static/js/runtime-main.26ddaa9e.js"
+  }
+]);
