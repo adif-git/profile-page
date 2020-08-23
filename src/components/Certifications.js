8bf1938ee69b199e2a45fde433145e829ebccc82
Adif <adif.ladiva@gmail.com>
2020-08-23 16:06:57 +0700

    Separate file for certifications list

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -1,30 +1,14 @@
 import React from 'react'
+import certsList from '../contents/Certifications'
 import './Certifications.css'
 
 class Certifications extends React.Component{
   render(){
-    const certList = [
-      {
-        id: '1',
-        title: 'Web Development Path (Node.js)',
-        organization: 'Progate'
-      },
-      {
-        id: '2',
-        title: 'Online Academy HTML, CSS, JavaScript',
-        organization: 'Kementerian Komunikasi dan Informatika RI'
-      },
-      {
-        id: '3',
-        title: 'Introduction to Cyber Security',
-        organization: 'Cisco'
-      },
-    ];
     return(
       <div className="container-fluid certifications-section">
         <h1 className="title"><span className="sign">&#187;</span>CERTIFICATIONS<span className="sign">&#171;</span></h1>
         <div className="row row-cols-1">
-            {certList.map((cert)=>{
+            {certsList.map((cert)=>{
               return(
                 <div key={cert.id}>
                   <h2>{cert.title}</h2>
---
+++ src/contents/Certifications.js
@@ -0,0 +1,19 @@
+const certsList = [
+    {
+      id: '1',
+      title: 'Web Development Path (Node.js)',
+      organization: 'Progate'
+    },
+    {
+      id: '2',
+      title: 'Online Academy HTML, CSS, JavaScript',
+      organization: 'Kementerian Komunikasi dan Informatika RI'
+    },
+    {
+      id: '3',
+      title: 'Introduction to Cyber Security',
+      organization: 'Cisco'
+    },
+  ];
+
+export default certsList
