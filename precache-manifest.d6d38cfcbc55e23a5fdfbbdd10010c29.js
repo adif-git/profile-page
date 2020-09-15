dd35ec6ea352b533dfda17f238f56ee4b950b2be
Adif <adif.ladiva@gmail.com>
2020-09-15 22:35:55 +0700

    Updates

---
+++ precache-manifest.d6d38cfcbc55e23a5fdfbbdd10010c29.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5264d7f448094d12f0944d2da536833",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "ff2a17444ba55fa529b2",
+    "url": "/profile-page/static/css/2.1d0e61fc.chunk.css"
+  },
+  {
+    "revision": "d916bc682965c6c8a2cc",
+    "url": "/profile-page/static/css/main.bab8d5e0.chunk.css"
+  },
+  {
+    "revision": "ff2a17444ba55fa529b2",
+    "url": "/profile-page/static/js/2.6e0c5f2e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/profile-page/static/js/2.6e0c5f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d916bc682965c6c8a2cc",
+    "url": "/profile-page/static/js/main.93c6da55.chunk.js"
+  },
+  {
+    "revision": "8a4dca0a4273f3bd0f4a",
+    "url": "/profile-page/static/js/runtime-main.a8128705.js"
+  },
+  {
+    "revision": "a4d6c57a9f1bf09ed042ee5cd1da988e",
+    "url": "/profile-page/static/media/Adif.a4d6c57a.jpg"
+  },
+  {
+    "revision": "93a48fd3bb949feb2e636ae7691b600f",
+    "url": "/profile-page/static/media/GKS.93a48fd3.jpg"
+  },
+  {
+    "revision": "8c3d5a6a7905cec2662f26f6ae005df1",
+    "url": "/profile-page/static/media/Goalkeeper.8c3d5a6a.png"
+  },
+  {
+    "revision": "1a2aee8719435b3c55ff1b87c09963b0",
+    "url": "/profile-page/static/media/Hexapod.1a2aee87.png"
+  },
+  {
+    "revision": "59e3c6bc741c80357218c0e4b2118ee7",
+    "url": "/profile-page/static/media/IT.59e3c6bc.png"
+  },
+  {
+    "revision": "692b7a9ecd053e4a38a1312362fc68d7",
+    "url": "/profile-page/static/media/Library.692b7a9e.png"
+  },
+  {
+    "revision": "f172a907a540fdf8a0f14cdc2c7761ca",
+    "url": "/profile-page/static/media/Profile.f172a907.png"
+  },
+  {
+    "revision": "66c8565ee5995c769d4cd0ada26268fb",
+    "url": "/profile-page/static/media/ProjectTracker.66c8565e.png"
+  },
+  {
+    "revision": "fe556b76b1bcfba700d6e72b17f382fd",
+    "url": "/profile-page/static/media/Quadcopter.fe556b76.jpg"
+  },
+  {
+    "revision": "f0a2be29149f2c26f6ab8fa269e348bc",
+    "url": "/profile-page/static/media/WaterTandon.f0a2be29.jpg"
+  }
+]);
